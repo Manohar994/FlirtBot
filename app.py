505b6419b952ab6fd19182696ae8598678f5e8d8
Manohara Sai Gubbala <maoharasai27072006@gmail.com>
2025-02-12 18:06:34 +0530

    Fixed duplicate Flask app issue

--- app.py
+++ app.py
@@ -17,13 +17,4 @@ def get_pickup_line():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
-from flask import Flask
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Hello, FlirtBot is live!"
-
-if __name__ == '__main__':
-    app.run()
